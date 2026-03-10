6c96ed41204dbaedc9688ea04c981b16938eaf3d
ai208 <yasu6328hand@gmail.com>
2026-03-10 16:50:57 +0900

    view の追加

--- repositories/routine_repository.py
+++ repositories/routine_repository.py
@@ -1,2 +1,4 @@
 # jsonの読み書き担当 モデル　→←　json
+# service → repositroy → storage 
+# 役割 : CRUD → save 開いた時に、　loadする
 # 3
--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -1,2 +1,4 @@
 # jsonの読み書き
+# service → repositroy → storage 
+# 役割 : CRUD → save 開いた時に、　loadする
 # 3
